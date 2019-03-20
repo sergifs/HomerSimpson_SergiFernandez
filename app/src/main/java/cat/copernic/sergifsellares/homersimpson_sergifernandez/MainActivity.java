9eb1a08c231e44530e5f1dcd61f775bc7630d368
sergifs <sergi.f.sellares@gmail.com>
2019-03-20 11:41:07 +0100

    Fet l'apartat D, engranatges amb animació

--- app/src/main/java/cat/copernic/sergifsellares/homersimpson_sergifernandez/MainActivity.java
+++ app/src/main/java/cat/copernic/sergifsellares/homersimpson_sergifernandez/MainActivity.java
@@ -4,12 +4,14 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView theSimpsons, homer, ull, donut, engranatge_vermell, engranatge_verd, engranatge_blau;
-
+    private Animation rotate_vermell, rotate_verd, rotate_blau;
     private Boolean isVisible = false;
 
     @Override
@@ -26,10 +28,17 @@ public class MainActivity extends AppCompatActivity {
         engranatge_blau = findViewById(R.id.engranatge_blau);
 
         ((AnimationDrawable) theSimpsons.getDrawable()).start();
+        rotate_vermell = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_vermell);
+        rotate_verd = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_verd);
+        rotate_blau = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_blau);
+
         theSimpsons.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (isVisible) {
+                    engranatge_vermell.clearAnimation();
+                    engranatge_verd.clearAnimation();
+                    engranatge_blau.clearAnimation();
                     ull.setVisibility(View.INVISIBLE);
                     donut.setVisibility(View.INVISIBLE);
                     engranatge_vermell.setVisibility(View.INVISIBLE);
@@ -42,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
                     engranatge_vermell.setVisibility(View.VISIBLE);
                     engranatge_verd.setVisibility(View.VISIBLE);
                     engranatge_blau.setVisibility(View.VISIBLE);
+                    engranatge_vermell.startAnimation(rotate_vermell);
+                    engranatge_verd.startAnimation(rotate_verd);
+                    engranatge_blau.startAnimation(rotate_blau);
                     isVisible = true;
                 }
             }
