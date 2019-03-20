60ad08faea1ee1bc14d7fd9ce394dbe8653f9937
sergifs <sergi.f.sellares@gmail.com>
2019-03-20 10:57:25 +0100

    Fet l'apartat B, titol i homer amb l'animació.

--- app/src/main/java/cat/copernic/sergifsellares/homersimpson_sergifernandez/MainActivity.java
+++ app/src/main/java/cat/copernic/sergifsellares/homersimpson_sergifernandez/MainActivity.java
@@ -1,13 +1,22 @@
 package cat.copernic.sergifsellares.homersimpson_sergifernandez;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView theSimpsons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        theSimpsons = findViewById(R.id.theSimpsons);
+        ((AnimationDrawable) theSimpsons.getDrawable()).start();
+
     }
 }
