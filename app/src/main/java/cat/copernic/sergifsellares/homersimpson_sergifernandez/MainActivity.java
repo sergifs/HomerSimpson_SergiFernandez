3ef8881fdd3c545eb7705a0db0e1e87ea2fbec45
sergifs <sergi.f.sellares@gmail.com>
2019-03-20 12:13:46 +0100

    Fet l'apartat E, donut amb les animacions acabades.

--- app/src/main/java/cat/copernic/sergifsellares/homersimpson_sergifernandez/MainActivity.java
+++ app/src/main/java/cat/copernic/sergifsellares/homersimpson_sergifernandez/MainActivity.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView theSimpsons, homer, ull, donut, engranatge_vermell, engranatge_verd, engranatge_blau;
-    private Animation rotate_vermell, rotate_verd, rotate_blau;
+    private Animation rotate_vermell, rotate_verd, rotate_blau, rotate_donut, translate_donut;
+    private AnimationSet donut_animation;
     private Boolean isVisible = false;
 
     @Override
@@ -32,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
         rotate_verd = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_verd);
         rotate_blau = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_blau);
 
+        //Donut
+        rotate_donut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_donut);
+        translate_donut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_donut);
+        translate_donut.setRepeatCount(-1);
+        translate_donut.setRepeatMode(Animation.REVERSE);
+        donut_animation = new AnimationSet(false);
+        donut_animation.addAnimation(rotate_donut);
+        donut_animation.addAnimation(translate_donut);
+
         theSimpsons.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
                     engranatge_vermell.clearAnimation();
                     engranatge_verd.clearAnimation();
                     engranatge_blau.clearAnimation();
+                    donut.clearAnimation();
                     ull.setVisibility(View.INVISIBLE);
                     donut.setVisibility(View.INVISIBLE);
                     engranatge_vermell.setVisibility(View.INVISIBLE);
@@ -54,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                     engranatge_vermell.startAnimation(rotate_vermell);
                     engranatge_verd.startAnimation(rotate_verd);
                     engranatge_blau.startAnimation(rotate_blau);
+                    donut.startAnimation(donut_animation);
                     isVisible = true;
                 }
             }
